80d366fa64a01ca9ab1186ec922cce059529d095
Alexandre Roman <aroman@pivotal.io>
2019-06-21 23:40:13 +0200

    Add observability

--- yatc-gateway/src/main/java/fr/alexandreroman/yatc/gateway/Application.java
+++ yatc-gateway/src/main/java/fr/alexandreroman/yatc/gateway/Application.java
@@ -16,27 +16,32 @@
 
 package fr.alexandreroman.yatc.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.*;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebFluxSecurity
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -54,30 +59,18 @@ public class Application {
     }
 }
 
-@Configuration
-class CorsConfiguration {
-    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN";
-    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
-    private static final String ALLOWED_ORIGIN = "*";
-    private static final String MAX_AGE = "3600";
+@Component
+class LoggingFilter implements GlobalFilter {
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
-    @Bean
-    public WebFilter corsFilter() {
-        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-            ServerHttpRequest request = ctx.getRequest();
-            if (CorsUtils.isCorsRequest(request)) {
-                ServerHttpResponse response = ctx.getResponse();
-                HttpHeaders headers = response.getHeaders();
-                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, ALLOWED_ORIGIN);
-                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHODS);
-                headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, MAX_AGE);
-                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, ALLOWED_HEADERS);
-                if (request.getMethod() == HttpMethod.OPTIONS) {
-                    response.setStatusCode(HttpStatus.OK);
-                    return Mono.empty();
-                }
-            }
-            return chain.filter(ctx);
-        };
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        final Set<URI> uris = exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
+        final String originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
+        final Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+        final URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+        log.info("Incoming request {} is routed to id: {}, uri: {}",
+                originalUri, route.getId(), routeUri);
+        return chain.filter(exchange);
     }
 }
